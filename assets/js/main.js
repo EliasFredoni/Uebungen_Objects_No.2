ca088dc15e055e5ebf66ca2a4e57a14f045848e4
EliasFredoni <fredoni.elias@gmail.com>
2022-05-27 14:59:55 +0200

    table

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/********LEV 2.1************/
+
+
+let edelMetallPreise = [
+    { name: "Gold", preiseGramEuro: 42.91, veraenderung: "+0.12%" },
+    { name: "Silber", preiseGramEuro: 0.51, veraenderung: "+1.02%" },
+    { name: "Platin", preiseGramEuro: 25.74, veraenderung: "+0.41%" },
+    { name: "Palladium", preiseGramEuro: 50.93, veraenderung: "0.00%" },
+    { name: "Rhodium", preiseGramEuro: 160.12, veraenderung: "-0.10%" },
+    { name: "Iridium", preiseGramEuro: 42.84, veraenderung: "0.00%" },
+    { name: "Ruthenium", preiseGramEuro: 7.36, veraenderung: "0.00%" },
+    { name: "Rhenium", preiseGramEuro: 37.22, veraenderung: "-0.53%" },
+    { name: "Osmium", preiseGramEuro: 11.54, veraenderung: "0.00%" }
+];
+
+edelMetallPreise.forEach((alle) => {
+    console.log(alle.name);
+    //console.log(alle.preiseGramEuro.push());
+    //consoel.log(alle.veraenderung.push());
+})
+
+edelMetallPreise.map((alle1) => {
+    console.log(alle1.name);
+    console.log(alle1.preiseGramEuro);
+    console.log(alle1.veraenderung);
+})
+
+edelMetallPreise.filter((alle3) => {
+    console.log(alle3.preiseGramEuro < 50);
+})
+
+/*********LEV 2.2***********/
+
+const singers = [
+    { name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" },
+    { name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
+    { name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B" },
+    { name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock" },
+    { name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica" },
+    { name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock" },
+    { name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop" },
+    { name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock" },
+];
+
+singers.sort((alle) => {
+    console.log(alle.name);
+})
